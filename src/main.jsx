f8ae9660d07263850896556c65e7870cf2c432fc
Matt Styles <matt@veryfizzyjelly.com>
2015-05-13 08:54:19 +0100

    #add main gradients

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ class App extends React.Component {
     render() {
         return (
             <body className="app">
+                <div className="app-bg"></div>
                 <IOSView />
             </body>
         )
