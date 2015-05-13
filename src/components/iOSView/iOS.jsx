a19ab078c6226648c02aeaa7b9f4dc63a5b618ff
Matt Styles <matt@veryfizzyjelly.com>
2015-05-13 10:10:04 +0100

    #add scrolling section

--- src/components/iOSView/iOS.jsx
+++ src/components/iOSView/iOS.jsx
@@ -10,11 +10,19 @@ export default class IOSView extends React.Component {
     }
 
     render() {
+        var items = []
+        for ( var i = 0; i < 60; i++ ) {
+            items.push( <div>{ i }</div> )
+        }
+
         return (
             <div className="iOS">
                 <div className="iOS-screen">
                     <StatusBar />
-                    <span>iOSView</span>
+                    <div className="iOS-mainView u-stretchY">
+                        <span>iOSView</span>
+                        { items }
+                    </div>
                 </div>
             </div>
         )
