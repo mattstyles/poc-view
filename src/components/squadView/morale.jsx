faa359295c2f80d057d3d272a8e4295fbfa2c705
mattstyles <matt@veryfizzyjelly.com>
2015-05-17 15:24:58 +0100

    #add morale background colour

--- src/components/squadView/morale.jsx
+++ src/components/squadView/morale.jsx
@@ -1,6 +1,7 @@
 
 import React from 'react'
 
+import { conditionGradient } from 'constants/gradients'
 import Icon from 'icon/icon'
 
 export default class MoraleIndicator extends React.Component {
@@ -13,12 +14,18 @@ export default class MoraleIndicator extends React.Component {
     }
 
     render() {
+        let style = {
+            background: conditionGradient[ ~~this.props.morale ]
+        }
+
+        // Morale rotation goes from 0 (straight up) to 180 (down)
         return (
-            <Icon
-                classes="Squad-Avatar-morale"
-                icon="ARROW_POINTER"
-                rotation={ -360 * ( this.props.morale / 100 ) - 90 }
-            />
+            <div style={ style } className="Squad-Avatar-morale">
+                <Icon
+                    icon="ARROW_POINTER"
+                    rotation={ -180 * ( this.props.morale / 100 ) + 90 }
+                />
+            </div>
         )
     }
 }
